fd8924e16229a9bc20ac0dde5de6666648eca056
Ignacio <ignaciolmartin@gmail.com>
2021-10-03 19:24:18 -0300

    create 404 ejs view

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.use('/', mainRouter);
 app.use('/products', productsRouter);
 // 404 Routes
 app.use((req, res, next) => {
-    res.status(404).render('not-found');
+    res.status(404).render('404-not-found');
     next();
 });
 
