17b159d9fc5a73130fe41728eabdf2762403a9d5
Ignacio <ignaciolmartin@gmail.com>
2021-10-03 19:13:44 -0300

    change staged

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -9,12 +9,32 @@ mongoose.connect('mongodb://localhost/gameshop', {
 }).then(() => {
     console.log("DB Connected!")
     console.log("--------------------")
-    console.log(Game.find({}))
+    console.log()
     console.log("--------------------")
-    /*const quotes = Game.find();
-    console.log(quotes)*/
+
 
 }).catch(err => {
     console.log(Error, err.message);
 });
 
+const nuevo = new Game({
+    slug: 'nuevo',
+    name: 'nuevo',
+    platforms: [{ 0: 'PC' }],
+    stores: null,
+    released: '15-25-2020',
+    background_image: 'none',
+    rating: 4.3,
+    rating_top: 5,
+    metacritic: 93,
+    clip: 'none',
+    tags: null,
+    esrb_rating: null,
+    short_screenshots: null,
+    genres: [{ 0: 'accion' }]
+});
+
+
+
+const quotes = Game.find();
+console.log(quotes);
