20a025351d462c5fb2100a021b0f8f7c82ad784d
Ignacio <ignaciolmartin@gmail.com>
2021-10-04 14:15:32 -0300

    testing navbar for mobile

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,15 @@
 const { validationResult } = require('express-validator');
 // Call UserSchema for MongoDB
 const userSchema = require('../models/userSchema');
+const gameSchema = require('../models/gameSchema');
+
+// Call FileSystem module
+const fs = require('fs');
+// Call Path module
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../database/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 // Create Main Controller
 const controller = {
--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,9 +1,26 @@
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
+document.onreadystatechange = () => {
+    if (document.readyState === 'loading') {
+        console.log('DOM is loading.');
+    }
+    else if (document.readyState === 'interactive') {
+        console.log('DOM is interactive.');
+    }
+    else if (document.readyState === 'complete') {
+        console.log('DOM is ready.');
+        cb();
+        console.log(cb());
+    }
+};
 
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+function cb() {
+
+
+    function openNav() {
+        document.getElementById("sidebar").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("sidebar").style.width = "0";
+    }
+
 }
