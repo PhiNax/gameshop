11c8833976164c88c6748557515305cadc9f989a
Ignacio <ignaciolmartin@gmail.com>
2021-10-04 15:02:35 -0300

    add navbar and sidebar for mobile

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,26 +1,7 @@
-document.onreadystatechange = () => {
-    if (document.readyState === 'loading') {
-        console.log('DOM is loading.');
-    }
-    else if (document.readyState === 'interactive') {
-        console.log('DOM is interactive.');
-    }
-    else if (document.readyState === 'complete') {
-        console.log('DOM is ready.');
-        cb();
-        console.log(cb());
-    }
-};
+function openNav() {
+    document.getElementById("sidebar").style.width = "210px";
+}
 
-function cb() {
-
-
-    function openNav() {
-        document.getElementById("sidebar").style.width = "250px";
-    }
-
-    function closeNav() {
-        document.getElementById("sidebar").style.width = "0";
-    }
-
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
 }
