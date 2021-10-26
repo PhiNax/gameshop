1f6fd94155de7e92e46d9e56432badfbf50ea242
Ignacio <ignaciolmartin@gmail.com>
2021-10-26 01:52:58 -0300

    Fix User Login/Register form views and userController
    Changes on Login and Register files:
        - Fix double head tag for
    
    Changes on Register file:
        - Fix req.body autocompletion when an error is encountered
        at registration validation
    
    Changes on userController:
        - Add async function to register new users
        - Check if register validation throws an error
        and show the error on register from view

---
+++ src/controllers/userController.js
@@ -0,0 +1,74 @@
+// Call result validation errors
+const { validationResult } = require('express-validator');
+// Call Bcrypt for encrypt passwords
+const bcrypt = require('bcrypt');
+// Call User Model 
+const { User } = require('../database/connectDB');
+
+const controller = {
+    // Cart
+    cart: (req, res) => {
+        res.render('users/cart');
+    },
+    // Login User
+    login: (req, res) => {
+        res.render('users/login');
+    },
+    // Login User
+    loginUser: async (req, res) => {
+        let email = req.body.email;
+        let password = req.body.password;
+        let passHash = bcrypt.hash(password, 10);
+
+        if (email && password) {
+            await User.query('SELECT * FROM users WHERE email = ?', [email])
+        }
+        res.render('/'), { user: User.firstName }
+    },
+    // Logout User
+    logoutUser: (req, res) => {
+        res.clearCookie('user')
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
+    },
+    // Register
+    register: (req, res) => {
+        res.render('users/register');
+    },
+    // Create New User from form
+    createUser: async (req, res) => {
+        const errors = validationResult(req);
+        console.log('--------------------------------')
+        console.log('--------------------------------')
+        console.log('--------------------------------')
+        console.log(errors.mapped());
+        console.log('--------------------------------')
+        console.log(errors.isEmpty())
+        console.log('--------------------------------')
+        if (errors.isEmpty()) {
+
+            let passCrypt = bcrypt.hashSync(req.body.password, 10);
+
+            let newUser = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: passCrypt,
+                agreeTerms: req.body.terms,
+                access: '1',
+            }
+            try {
+                await User.create(newUser);
+                res.redirect('/');
+            }
+            catch (err) {
+                throw 'Create New User: Error => ' + err;
+            }
+        } else {
+            res.render('users/register', { errors: errors.mapped(), old: req.body });
+        }
+    }
+}
+
+module.exports = controller;
