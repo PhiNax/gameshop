9cb3db230309d2947047c8b6539c95ecbb7e78f1
Ignacio <ignaciolmartin@gmail.com>
2021-10-28 01:59:51 -0300

    Fix errors on Express Validator
        - Remove sanitizied options
        - Add a doble password validator

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -4,39 +4,40 @@ const { check } = require('express-validator');
 const validateRegister = [
     check('firstName')
         .notEmpty().withMessage('Must complete your firstname')
-        .isLength({ min: 3 }).trim().withMessage('Your firstname must contain minimium 3 characters'),
+        .isLength({ min: 3 }).withMessage('Your firstname must contain minimium 3 characters'),
 
     check('lastName')
         .notEmpty().withMessage('Must complete your lastname')
-        .isLength({ min: 3 }).trim().withMessage('Your lastname must contain minimium 3 characters'),
-
+        .isLength({ min: 3 }).withMessage('Your lastname must contain minimium 3 characters'),
 
     check('email')
         .notEmpty().withMessage('Must complete your email')
-        .isEmail().trim().escape().normalizeEmail().withMessage('Must be a valid email'),
-
+        .isEmail().withMessage('Must be a valid email'),
 
     check('password')
         .notEmpty().withMessage('Must complete your password')
-        .isLength({ min: 8 }).trim().escape().withMessage('Password must contain minimun 8 characters'),
+        .isLength({ min: 8 }).escape().withMessage('Password must contain minimun 8 characters'),
 
     check('passwordC')
-        .notEmpty().equals().withMessage('Password do not match')
-
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password do not match');
+            }
+            return true;
+        })
     /*
     check('agreeTerm')25
         .notEmpty().withMessage('Must agree with user terms and conditions')
-    */
+   */
 ]
 
 const validateLogin = [
     check('email')
-        .notEmpty().withMessage('Must complete your email').bail()
+        .notEmpty().withMessage('Must complete your email')
         .isEmail().withMessage('Must be a valid email'),
 
     check('password')
-        .notEmpty().withMessage('Must complete your password').bail()
-        .isLength({ min: 8 }).withMessage('Password must contain minimun 8 characters')
+        .notEmpty().withMessage('Must complete your password')
 ]
 
 module.exports = validateRegister, validateLogin;
