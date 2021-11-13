ecb2f0ac29d34c01d51a45df4b3255c0270108a1
Ignacio <ignaciolmartin@gmail.com>
2021-11-13 01:47:49 -0300

    Change on authMiddleware
        - authMiddleware check if the user is logged in and
        redirect to the user profile page
    
        - guestMiddleware check if the user is not logged in and
        redirect to login page

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,13 +1,14 @@
 function authMiddleware(req, res, next) {
-    if (!req.session.userLogged) {
-        return res.redirect('/login');
+    if (req.session.userLogged) {
+        return res.redirect('/user/profile');
     }
     next();
 }
 
+// Check if there a session with the given property userLogged
 function guestMiddleware(req, res, next) {
     if (!req.session.userLogged) {
-        return res.redirect('/login');
+        return res.redirect('/user/login');
     }
     next();
 }
