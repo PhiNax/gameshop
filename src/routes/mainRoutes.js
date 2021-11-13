5f02dbd9fc86c957a5726c869290a039c36c7fcf
Ignacio <ignaciolmartin@gmail.com>
2021-11-13 01:50:57 -0300

    Change on mainRoutes
        - Delete all routes linked to the user routes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -2,26 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const mainController = require('../controllers/mainController');
-const userController = require('../controllers/userController');
-
-const { validateRegister, validateLogin } = require('../middleware/formValidator');
-
-const { authMiddleware, guestMiddleware } = require('../middleware/userAuth');
 
 // Set Routes
 // Index Route
 router.get('/', mainController.index);
-// Cart Details Route
-router.get('/cart', guestMiddleware, userController.cart);
-// Login Route
-router.get('/login', userController.login);
-// Login User
-router.post('/login', validateLogin, userController.loginUser);
-// Logout User
-router.post('/logout', userController.logoutUser);
-// Register Route
-router.get('/register', userController.register);
-// Create New User
-router.post('/register', validateRegister, userController.createUser);
 
 module.exports = router;
